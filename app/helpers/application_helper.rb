6aa5719cab05844bd9d2e7bc953f4ead88e586f3
AbeKazumasa <katahira0088@gmail.com>
2021-01-19 22:03:22 -0600

    20210119

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+
+  def full_title(page_name = "")
+    base_title = "Daily_Management_App"
+     if page_name.empty?
+      base_title
+     else
+      page_name + " | " + base_title      
+     end
+  end
+
 end
